26be565a9ed76ce2df9e68da80c1309b610f8924
Jaren-Wheeler <83668434+Jaren-Wheeler@users.noreply.github.com>
2026-01-20 00:02:19 -0700

    Restore proper dependencies. Include route mounting for chatbot.

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const userRoutes = require("./routes/User.routes");
 const schedulerRoutes = require("./routes/scheduler.routes");
 const notesRoutes = require("./routes/notes.routes");
 const financeRoutes = require("./routes/finance.routes");
+const chatbotRoutes = require("./routes/chatbot.routes");
 
 app.use(cors());
 app.use(express.json());
@@ -18,5 +19,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/scheduler", schedulerRoutes);
 app.use("/api/notes", notesRoutes);
 app.use("/api/finances", financeRoutes);
+app.use("/api/chatbot", chatbotRoutes);
 
 module.exports = app;
---
+++ backend/src/routes/chatbot.routes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const chatboatController = require("../controllers/chatbot.controller");
+const router = express.Router();
+
+module.exports = router;
