796696009c5eea24607c7b93d8392f2b7c5b04f8
Jaren-Wheeler <83668434+Jaren-Wheeler@users.noreply.github.com>
2026-01-20 00:46:29 -0700

    Include rate limits for chtabot API call to 20 per minute maximum

--- backend/src/routes/chatbot.routes.js
+++ backend/src/routes/chatbot.routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const chatbotController = require("../controllers/chatbot.controller");
 const router = express.Router();
 
-router.post("/", chatbotController.chatbotConversation);
+const chatbotController = require("../controllers/chatbot.controller");
+const { chatRateLimiter } = require("../middleware/rateLimits");
+/**
+ * @route POST api/chat
+ * @desc Write chats to call the OpenAI API. Limited by chatRateLimiter to 20 calls per minute.
+ * @access Private
+ */
+router.post("/", chatRateLimiter, chatbotController.chatbotConversation);
 
 module.exports = router;
